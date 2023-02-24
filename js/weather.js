e126b69b4af0295dd85bcac9b126e993f3f497b2
Auroraee <2744243587@qq.com>
2023-02-24 10:57:27 +0800

    Site updated: 2023-02-24 10:57:27

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "0124",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "9px 10px 9px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "f9908e84c2364c56b92440efeb0e4d08"
+  }
+}
